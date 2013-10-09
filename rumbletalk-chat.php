a49d505fa22e48b19f3faf5e74300c6e372a2070
RumbleTalk <RumbleTalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-09 21:33:28 +0000

    version update
    git-svn-id: https://plugins.svn.wordpress.org/rumbletalk-chat-a-chat-with-themes/tags/2.1.0@785550 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rumbletalk-chat.php
+++ rumbletalk-chat.php
@@ -3,7 +3,7 @@
   Plugin Name: RumbleTalk Chat
   Plugin URI: http://www.rumbletalk.com/3rdsupport.php
   Description: Add a <strong>Boutique Chatroom Widget</strong> to your blog or site in under a minute
-  Version: 1.3.1
+  Version: 2.1.0
   Author: Yanir Shahak
   Author URI: http://www.rumbletalk.com/contact_us.php
   License: GPL2
