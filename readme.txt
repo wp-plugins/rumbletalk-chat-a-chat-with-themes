b6d5dd8cd0f1f564f8394cb652143c4ae6d75d6f
RumbleTalk <RumbleTalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-03 12:47:12 +0000

    Minor update
    git-svn-id: https://plugins.svn.wordpress.org/rumbletalk-chat-a-chat-with-themes/tags/1.0.4@942832 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,130 @@
+﻿=== HTML5 Online CHAT ROOM Widget ===
+Contributors: yanir
+Donate link:
+Tags: chat, discussion, html5, widget, plugin, live, comments, forums, chat widget
+Requires at least: 2.0.2
+Tested up to: 3.5.1
+Stable tag: trank
+
+
+Boutique Social Chatroom plugin - FREE Trial!
+
+== Description ==
+
+A hosted professional stylish **chat plugin** to connect you with your website,blog or real-time event visitors.
+[Visit Site >>](http://www.rumbletalk.com/?utm_source=Wordpress&utm_medium=site&utm_campaign=Wordpress) :
+
+[youtube http://www.youtube.com/watch?v=T9d7ZhUuJZg]
+
+RumbleTalk is perfectly integrated with facebook and twitter.
+A site owner can choose their own chat theme from a variety of ready made themes and even design one.
+It is the fastest way to add a unique fully functional chat to your blog or website.
+
+
+<pre>
+* Theme Library
+* Talk from Mobile and Tablet.
+* Login, Share Invite using Facebook and Twitter.
+* Private chat
+* One chat for your WP and facebook page.
+* Design your own chat theme.
+* Design your chat with css (for advanced users)
+* Single sign-on.
+* Manage as many chats as you like.
+* Spam filter (create a black listed words).
+* Ban, Delete Trolls.
+* Define moderators and rolls.
+* Archive your chat, Save log of your chat history.
+* Chat in 30 languages.
+* Offline Mode
+* new - Delete single messages
+</pre>
+
+<pre>
+**Languages**
+English, Español, Bahasa Indonesia, Català, Czech, Dansk sprog
+Deutsch, Français, Italiano, Magyar, فارسی, Nederlands, dutch, Polski
+Português, Română, ภาษาไทย, Slovenčina, Suomi, Svenska, Tiếng Việt
+Türkçe, ελληνικά, Български, Македонски, Русский язык
+Србски, Українська, עברית , عربي
+</pre>
+
+
+![RumbleTalk chat themes](http://d1pfint8izqszg.cloudfront.net/images/mainpage/girl2.png)
+
+== Installation ==
+
+**First, Get your chat code:**
+
+1. Enter your email & choose a password at http://www.rumbletalk.com
+2. Then click on the "Get Chat Code" and get the code for wordpress.
+
+**Second, Install the Plugin:**
+ 
+3. Upload the rumbletalk-chat directory to the `/wp-content/plugins/` directory
+4. Activate the plugin through the 'Plugins' menu in WordPress
+5. Click 'Settings' and then 'RumbleTalk chat'
+6. Add a the chat code. (Optional - Enter the chat width and height e.g 400 x 300)
+
+**Add the chat to your chosen page:**
+
+7. Use the phrase '[rumbletalk-chat]' in your posts text (you can do it in visual mode, no 
+need to switch to HTML view) in order to position the chat widget exactly where you need it.
+
+
+Full instructions can be found here:
+
+http://www.rumbletalk.com/wordpress-chat-plugin.php
+
+
+== Screenshots ==
+1. Chat with your website visitors and add videos and images to the conversation.
+2. Theme - new age theme, design your own chat theme in 1 minute.
+4. Theme - Art theme, give a stylish chat to your organization
+5. Theme - Control any aspect of the chat, fonts, borders, image backgrounds and much more.
+6. Mobile chat (Tablet & smart phones). A chat to go, keep talking on the road.
+7. Floating chat - Example of a floating toolbar chat that will open when clicked. You can change this Icon to your liking.
+
+
+== Frequently Asked Questions ==
+
+
+= Can i chat with my ipad or smart phone?  =
+
+Sure, you can add the chat to your website or blog and talk from anywhere.
+
+= Can i manage more than a single chat in my account?  =
+
+Yes, RumbleTalk is group oriented. This means you define a group (logical name) and within a group you define as many 
+chats as you like. You can integrate it on different domains.
+
+= I want my users to login only with Facebook logins, is it possible? =
+Sure. As admin you can decide how users will login. 
+Only as facebook users, as anonymous users, with user names and passwords you define for them or any combination of the 3. 
+
+= Can I have different designs for different chats? =
+Yes you can, For each chat choose a design or create a design with our chat template builder.
+
+= My chat height is too small? =
+
+RumbleTalk chat is elastic chat. It means it will consume
+all the space around it. If that does not work try to use a direct embed (see next question how)
+
+= Can I use direct embed in an html page? =
+ 
+Sure, you can go to Source of your page and copy paste a code that looks like that.
+Just replace the "chatcode" with your own 8 digit code.
+
+<pre>
+Example: 
+
+&#60;div style="width: 100px; height: 200px;"&#62;
+&#60;script language="JavaScript" type="text/javascript" src="http://www.rumbletalk.com/client/?chatcode"&#62;&#60;/script&#62;
+&#60;/div&#62;
+</pre>
+
+
+= Can I delete signle messages?  =
+
+Sure, login as admin to the chat and see tray icon next to each message, clicking on it will delete the chat.
+
---
+++ rumbletalk-chat.php
@@ -0,0 +1,169 @@
+<?php
+/*
+Plugin Name: RumbleTalk Chat
+Plugin URI: http://www.rumbletalk.com/3rdsupport.php
+Description: Add a <strong>Free Group Chat Widget</strong> to your blog or site in under a minute
+Version: 0.1
+Author: Yanir Shahak
+Author URI: http://www.rumbletalk.com/contact_us.php
+License: GPL2
+
+Copyright 2011 Yanir Shahak (email : yanir@rumbletalk.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+	class RumbleTalkChat
+	{
+		protected $options;
+
+		public function __construct()
+		{
+			$this->options = array
+			(
+				"rumbletalk_chat_code",
+				"rumbletalk_chat_width",
+				"rumbletalk_chat_height"
+			);
+
+			register_activation_hook( __FILE__, array( &$this, "install" ) );
+			register_deactivation_hook( __FILE__, array( &$this, "unInstall" ) );
+
+			if ( is_admin() )
+			{
+				add_action( "admin_menu", array( &$this, "adminMenu" ) );
+			}
+			else
+			{
+				add_filter( "the_content", array( &$this, "embed" ) );
+			}
+		}
+
+		public function adminMenu()
+		{
+			add_options_page
+			(
+				"RumbleTalk Chat",
+				"RumbleTalk Chat",
+				"administrator",
+				"rumbletalk-chat",
+				array
+				(
+					&$this,
+					"drawAdminPage"
+				)
+			);
+		}
+
+		public function drawAdminPage()
+		{
+?>			
+			<div>
+				<h2>RumbleTalk Chat Options</h2>
+				<form method="post" action="options.php">
+					<input type="hidden" name="action" value="update"/>
+					<input type="hidden" name="page_options" value="rumbletalk_chat_code,rumbletalk_chat_width,rumbletalk_chat_height"/>
+					<? wp_nonce_field( "update-options" ); ?>
+					<table>
+						<tr>
+							<td>Chatroom code:</td>
+							<td><input type="text" name="rumbletalk_chat_code" id="rumbletalk_chat_code" value="<?= htmlspecialchars( get_option( "rumbletalk_chat_code" ) ); ?>" maxlength="8"/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>
+								This is the 8 letters/signs chat room code you've received from RumbleTalk.<br/>
+								If you do not have a chat room code, please register at <a href="http://www.rumbletalk.com/" target="_blank">RumbleTalk.com</a>, it's super fast and free.
+							</td>
+						</tr>
+						<tr>
+							<td>Chatroom width:</td>
+							<td><input type="text" name="rumbletalk_chat_width" id="rumbletalk_chat_width" value="<?= htmlspecialchars( get_option( "rumbletalk_chat_width" ) ); ?>" maxlength="4"/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>
+								The width in pixels you wish the chat widget to be.<br/>
+								You can use percentages (e.g. 40%) or leave blank.
+							</td>
+						</tr>
+						<tr>
+							<td>Chatroom height:</td>
+							<td><input type="text" name="rumbletalk_chat_height" id="rumbletalk_chat_height" value="<?= htmlspecialchars( get_option( "rumbletalk_chat_height" ) ); ?>" maxlength="4"/></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td>
+								The height in pixels you wish the chat widget to be.<br/>
+								You can use percentages (e.g. 40%) or leave blank.
+							</td>
+						</tr>
+						<tr>
+							<td colspan="2"><input type="submit" value="<? _e( "Save Changes" ) ?>"/></td>
+						</tr>
+					</table>
+				</form>
+			</div>
+<?
+		}
+		
+		public function embed( $content )
+		{
+			$code = get_option( "rumbletalk_chat_code" );
+			
+			if ( empty( $code ) )
+			{
+				return;
+			}
+
+			$width  = get_option( "rumbletalk_chat_width"  );
+			$height = get_option( "rumbletalk_chat_height" );
+			$isw    = ( preg_match( "/^\d{1,4}%?$/", $width ) == 1 );
+			$ish    = ( preg_match( "/^\d{1,4}%?$/", $height ) == 1 );
+			$str    = "";
+
+			if ( $isw || $ish )
+			{
+				$str = "<div style=\"" . ( $isw ? "width: {$width}px;" : "" ) . ( $ish ? "height: {$height}px;" : "" ) . "\">";
+			}
+
+			$str .= "<script type=\"text/javascript\" src=\"http://www.rumbletalk.com/client/?$code\"></script>";
+
+			if ( $isw || $ish )
+			{
+				$str .= "</div>";
+			}
+
+			return preg_replace( "/\[rumbletalk-chat\]/i", $str, $content );
+		}
+
+		public function install()
+		{
+			foreach ( $this->options as $opt )
+			{
+				add_option( $opt );
+			}
+		}
+
+		public function unInstall()
+		{
+			foreach ( $this->options as $opt )
+			{
+				delete_option( $opt );
+			}
+		}
+	}
+
+	new RumbleTalkChat();
+?>
